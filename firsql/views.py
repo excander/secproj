4eddc241635528d0ffba0d0e472d2b9042dcb2cb
excander <excander@gmail.com>
2019-02-18 00:40:25 +0300

    Перед изменением url-ов на re_path

--- firsql/views.py
+++ firsql/views.py
@@ -5,7 +5,7 @@ from .models import User
 
 
 def index(request):
-    return HttpResponse("The index page!")
+    return render(request, 'firsql/index.html')
 
 def users_all(request):
     user_list = User.objects.all()
@@ -14,4 +14,8 @@ def users_all(request):
 
 def user_detail(request, user_id):
     user = get_object_or_404(User, pk=user_id)
+    return render(request, 'firsql/user_detail.html', {'user': user})
+
+def user_detail_by_login(request, user_login):
+    user = User.objects.filter(login="admin")[0]
     return render(request, 'firsql/user_detail.html', {'user': user})
