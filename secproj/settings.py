b8f7cf0f2568bed4d8623519eeb4aa2e6351b92c
excander <excander@mail.ru>
2020-07-30 19:33:48 +0300

    Update settings.py
    secretkey keeped in secret

--- secproj/settings.py
+++ secproj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h97=#g!uwu#g%byy3dv43sdz-5ljo@yjfj3i#cu5af%%bvzn^('
+SECRET_KEY = 'keeped in secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
